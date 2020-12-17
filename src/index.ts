fd4653e33f340c970eec244d78bad33d00816dbc
root <root@DESKTOP-H022FR1.localdomain>
2020-12-17 17:43:30 +0900

    20201217

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+interface Human{
+    name:string;
+    age:number;
+    gender:string;
+}
 
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+const person = {
+    name: "NB2",
+    age:22,
+    gender: "male"
 };
 
-console.log(sayHi("NB", 31, "maale"));
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
 
 export {};
